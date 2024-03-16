4a7682211e96c44371cfa592fa41d66cc3688d19
_ñ <knaka0209@gmail.com>
2024-03-17 08:45:41 +0900

    up, 0317a5

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,12 +6,11 @@ function Page() {
     <div>
         <Link to="/">Home</Link>
         <Link to="/about">&nbsp; [ about ]</Link>
-        <Link to="/test4">&nbsp; [ test4 ]</Link>
         <hr />
     </div>
     );
 }
 export default Page;
 /*
-<Link to="/test1">&nbsp; [ test1 ]</Link>
+<Link to="/test4">&nbsp; [ test4 ]</Link>
 */
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
 
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import { renderToString } from 'react-dom/server';
 const app = express();
+import 'dotenv/config'
 //
 import Top from './pages/App';
 //
-//import Common from './lib/Common';
-import testRouter from './routes/test'; 
-import commonRouter from './routes/common';
+//import testRouter from './routes/test'; 
+//import commonRouter from './routes/commonRouter';
 //
-app.use(cors())
+//app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-console.log("env=", process.env.NODE_ENV);
-//console.log("VITE_API_URL=", import.meta.env.VITE_API_URL);
+console.log("env=", process.env.NODE_ENV)
+console.log("EXTERNAL_API_URL=", process.env.EXTERNAL_API_URL)
 //
 const errorObj = {ret: "NG", messase: "Error"};
-
 // route
-app.use('/api/test', testRouter);
-app.use('/api/common', commonRouter);
-//SPA
+//app.use('/api/common', commonRouter);
 app.get('/*', (req: any, res: any) => {
   try {
     res.send(renderToString(Top()));
@@ -32,9 +29,8 @@ app.get('/*', (req: any, res: any) => {
 });
 
 //start
-if (import.meta.env.PROD) {
-  app.listen(3000);
-}
+const PORT = 4000;
+app.listen({ port: PORT }, () => {
+  console.log(`Server ready at http://localhost:${PORT}`);
+});
 console.log('start');
-
-export const viteNodeApp = app;
