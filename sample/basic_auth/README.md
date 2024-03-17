9f3ba496dd3a58c7850886eb2713a7cffbb487f2
_ñ <knaka0209@gmail.com>
2024-03-17 11:06:12 +0900

    up, 0317a6

---
+++ sample/basic_auth/README.md
@@ -0,0 +1,26 @@
+﻿# basic_auth
+
+ Version: 0.9.2
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2024/01/25
+
+ update :
+
+***
+
+auth  sample
+
+***
+* index.ts: user, password set
+
+```
+app.use(basicAuth({
+  users: { "test": "1111" },
+  challenge: true,
+}));
+```
+
+***
+
---
+++ sample/basic_auth/src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+//
+import Home from './client/home';
+import About from './client/about';
+//
+export default function App(){
+    return(
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+    )
+}
+/*
+<div className="App">src/App.tsx
+*/
