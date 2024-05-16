62bd5ca9c789c8e306c5c1887030119f32ca76a9
_ñ <knaka0209@gmail.com>
2024-05-16 19:20:01 +0900

    up, 0516a6

--- sample/zod/src/components/DialogBox.tsx
+++ sample/zod/src/components/DialogBox.tsx
@@ -2,20 +2,31 @@
 function Compo(props: any) {
 console.log(props);
   //
+  const closeButton = function(){
+    const dlg = document.getElementById('modalDialog');
+    if(dlg) {
+      //@ts-ignore
+      dlg.close();
+    }
+  }
+  //
   return (
   <dialog id="modalDialog" className="dialog">
-    <div className="bg-white px-8 pt-3 pb-1 dialog_body_wrap">
+    <div className="bg-white px-8 pt-3 pb-3 dialog_body_wrap">
       <p className="text-3xl font-bold">{props.message}</p>
       <hr className="my-3" />
-      <form method="dialog mb-1">
-        <button 
+      <button onClick={()=>closeButton()}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-8 rounded"
         type="submit" value="OK">Ok</button>
-      </form>
     </div>
   </dialog>
   );
 }
 export default Compo;
 /*
+<form method="dialog mb-1">
+  <button 
+  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-8 rounded"
+  type="submit" value="OK">Ok</button>
+</form>
 */
--- sample/zod/src/components/ErrorDialogBox.tsx
+++ sample/zod/src/components/ErrorDialogBox.tsx
@@ -2,20 +2,31 @@
 function Compo(props: any) {
 console.log(props);
   //
+  const closeButton = function(){
+    const dlg = document.getElementById('errorModalDialog');
+    if(dlg) {
+      //@ts-ignore
+      dlg.close();
+    }
+  }
+  //
   return (
   <dialog id="errorModalDialog" className="dialog">
-    <div className="bg-white px-8 pt-3 pb-1 dialog_body_wrap">
+    <div className="bg-white px-8 pt-3 pb-3 dialog_body_wrap">
       <p className="text-3xl font-bold text-red-400">{props.message}</p>
       <hr className="my-3" />
-      <form method="dialog mb-1">
-        <button 
+      <button onClick={()=>closeButton()}
         className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-8 rounded"
         type="submit" value="OK">Ok</button>
-      </form>
     </div>
   </dialog>
   );
 }
 export default Compo;
 /*
+<form method="dialog mb-1">
+  <button 
+  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-8 rounded"
+  type="submit" value="OK">Ok</button>
+</form>
 */
