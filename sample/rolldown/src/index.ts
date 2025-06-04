8ea526ea9b3955227df69419f5578132d06bea7f
中島浩二 <knaka0209@gmail.com>
2025-06-04 17:42:17 +0900

    up, 0604a8

--- sample/rolldown/src/index.ts
+++ sample/rolldown/src/index.ts
@@ -3,10 +3,9 @@ import express from 'express';
 import { renderToString } from 'react-dom/server';
 const app = express();
 import 'dotenv/config'
-//
+
 import Top from './pages/App';
-//
-//import testRouter from './routes/test'; 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -14,7 +13,7 @@ console.log("env=", process.env.NODE_ENV)
 console.log("EXTERNAL_API_URL=", process.env.EXTERNAL_API_URL)
 //
 const errorObj = {ret: "NG", messase: "Error"};
-// route
+
 //app.use('/api/common', commonRouter);
 app.get('/*', (req: any, res: any) => {
   try {
